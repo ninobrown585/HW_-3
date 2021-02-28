6d4e35e8384280dd1e7f4fff534f28508151561e
NinoB <adrian.brown1@g.austincc.edu>
2021-02-27 21:30:46 -0600

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  var generateBtn = button.queryselector('#generate');
+  var generateBtn = document.getElementById('generate');
   
   // Array of uppercase characters to be included in password
   var capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -7,7 +7,7 @@
   // Array of special characters to be included in password
   var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',', ')','(','}','{',']','[','~','-','_','.'];
   // Array of numeric characters to be included in password
-  var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   function btnClicked() {
     // ask user for input length and store input into passwordLen//
@@ -22,20 +22,58 @@
       };
       // Users choice being stored in variables //
       alert('please choose atleast one ' )
-      var capLet = confirm('WOuld you like capital letters included?');
+      var capLet = confirm('WOuld you like capital letters included?');      
       var lowerLet = confirm('WOuld you like lowercase letters included?');
       var specialPwdChar = confirm('WOuld you like Special Characters included?');
       var pwdNum = confirm('WOuld you like numbers included?');
 
 
-      
+      /*detects if the user doesn't make an input */
       if (capLet===false && lowerLet=== false && specialPwdChar=== false && pwdNum=== false) {
         alert('no input detected');
       };
 
+      /*stores chosen options with the values of those options*/
+      var passwordPool =  {
+        capLet: capLet,
+        lowerLet: lowerLet,
+        specialPwdChar: specialPwdChar,
+        pwdNum: pwdNum,
+        passwordLen: passwordLen
+      };
+      /* random function generator */
+      function chooseRandom(getRand) {
+        var indexContained = Math.floor(Math.random() * getRand.length);
+        var ranIndex = getRand[indexContained];
+        return ranIndex;
+      }
+
+      function passwordSearch () {
+        var searchChoices = [];
+
+        var finalRandomChosen = [];
+
+        if(passwordPool.capLet) {
+          finalRandomChosen.push(chooseRandom(capLetters));
+        };
+
+        if(passwordPool.lowerLet) {
+          finalRandomChosen.push(chooseRandom(lowerLetters));
+        };
+
+        if(passwordPool.specialPwdChar) {
+          finalRandomChosen.push(chooseRandom(specialChar));
+        };
+
+        if(passwordPool.pwdNum) {
+          finalRandomChosen.push(chooseRandom(numberList));
+        };
+        
+      };
+      console.log(finalRandomChosen);
     };
   
-    //generateBtn.addevenlistener('click', btnClicked);//
+    
 
 
 
